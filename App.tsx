94d6f69cb16c9546eca4e3e23722875737a4478d
LAPTOP-864TU38I\Кирилл <mrralio@mail.ru>
2024-06-02 23:15:29 +0300

    theme and style complete

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from "react-native";
+import { ThemeProvider } from "@shopify/restyle";
+import { StatusBar } from "expo-status-bar";
+import { TouchableOpacity, View } from "react-native";
+import theme, { Box, Text } from "utils/theme";
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>Hello world!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <StatusBar style="dark" />
+        <Text variant="text3Xl">Hello world!</Text>
+      </View>
+    </ThemeProvider>
   );
 };
 
